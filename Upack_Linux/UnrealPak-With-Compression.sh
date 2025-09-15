3932e772bd6b8f6839cbbc9b5317609dbf17571d
WK-Kz <molaskendall@gmail.com>
2025-09-15 00:40:50 -0700

    Fix error with Unreal Script

--- Upack_Linux/UnrealPak-With-Compression.sh
+++ Upack_Linux/UnrealPak-With-Compression.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
-if [ $# -ne 0 ]; then
-  # echo "Error"
+#!/bin/bash
+if [ $# -eq 0 ]; then
+  echo "Error"
   exit
 fi
 
 pushd .
-echo $1/*.* "../../../*.*" > filelist.txt
+echo "Pushing directory"
+cd Upack_Linux
 ./UnrealPak $1.pak -create=filelist.txt -compress
 popd
 
