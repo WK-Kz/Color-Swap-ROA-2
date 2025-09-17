6a243ca2aae5a8f097fa3085c13861a1af8d5db3
WK-Kz <molaskendall@gmail.com>
2025-09-16 19:21:58 -0700

    Start.sh: Update to check for wine in system path and flatpak directory
    It was discovered that in order to install wine on a steam deck, the
    user must install wine via flatpak.
    
    However, this also addresses another edge case for users who install
    wine via flatpak and don't install it via system packages.

--- Start.sh
+++ Start.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
 if command -v python3 &>/dev/null; then
-  echo "Python 3 is installed."
+  echo "[INFO] - SUCCESS -- Python 3 is installed."
+  if command -v wine &>/dev/null; then
+    echo "[INFO] - SUCCESS -- Wine has been detected in path."
+  else
+    echo "[INFO] - WARNING -- Wine is not detected in path, checking flatpak path"
+    WINE_PACKAGE=org.winehq.Wine
+    flatpak info ${WINE_PACKAGE} >/dev/null 2>&1
+    if [ $? -eq 0 ]; then 
+      echo "[INFO] Wine has been detected in the user's flatpak directory"
+    else
+      echo "[INFO] Wine flatpak was not detected on your system, would you like to install it?[y/n]"
+      read response
+
+      if [ "$response" == 'y' ]; then
+        echo "[INFO] Running command to install wine."
+        echo "[INFO] Follow the prompts to install the latest **STABLE** wine"
+        sleep 1
+        flatpak install flathub org.winehq.Wine
+      else
+        echo "[Error] - Exiting -- Valid response was not chosen. Please install wine via flatpak or from your package manager."
+        exit
+      fi
+    fi
+  fi
 else
-  echo "Python 3 is not installed"
-  echo "Exiting"
+  echo "[Error] - Exiting -- Python 3 is not installed"
   exit
 fi
 
